fbbb7556b0f16798db56df6fab490b9971858bff
Carl Greenwood <carl.greenwood@gmail.com>
2012-05-07 11:51:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlepiratesbot::Application.config.secret_token = '3a05e35465e7fffa0e750b9df719f72f39b9dadfc41ff5ef2191310dc4ee416c406ea59a600de4f5791cea2d64df22c134797ab9e00f78682af851cb3e0abba0'
